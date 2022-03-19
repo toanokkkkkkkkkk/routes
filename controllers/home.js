7ed62d109320eea947eaa597ca32de5f9a2e9d92
toanokkkkkkkkkk <98519580+toanokkkkkkkkkk@users.noreply.github.com>
2022-03-19 22:37:58 +0700

    Update home.js

--- controllers/home.js
+++ controllers/home.js
@@ -1,7 +1,7 @@
 const { errorHandler } = require("../utils");
 exports.home = async (req, res, next) => {
   var hi = {}
-      hi.baucua = `/baucuabaucua?chon= [ bầu - cua - cá - nai - gà - tôm ]`
+      hi.baucua = `/baucua?chon= [ bầu - cua - cá - nai - gà - tôm ]`
       hi.Authors = `[ Tòn ] - Dưa Hấu`
 res.header("Content-type", "application/json; charset=utf-8")
 res.send(JSON.stringify(hi, null, 2))
