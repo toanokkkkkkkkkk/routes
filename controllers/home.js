3ad60513ac8674558b0bdcfe4bc497a010b4e6f1
Lailatondayy <98519580+toanokkkkkkkkkk@users.noreply.github.com>
2022-03-27 12:04:25 +0700

    :>

--- controllers/home.js
+++ controllers/home.js
@@ -2,6 +2,7 @@ const { errorHandler } = require("../utils");
 exports.home = async (req, res, next) => {
   var hi = {}
       hi.baucua = `/baucua?chon= [ bầu - cua - cá - nai - gà - tôm ]`
+      hi.taixiu = `/taixiu?chon= [ tài / xỉu ]`
       hi.Authors = `[ Tòn ] - Dưa Hấu`
 res.header("Content-type", "application/json; charset=utf-8")
 res.send(JSON.stringify(hi, null, 2))
