912bab1cf86f61c186260283b8a679692edde8a2
Lailatondayy <98519580+toanokkkkkkkkkk@users.noreply.github.com>
2022-03-27 12:05:37 +0700

    :>

--- routes/api.js
+++ routes/api.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 
 const { home } = require("../controllers/home");
 const { baucua } = require("../controllers/baucua");
+const { taixiu } = require("../controllers/taixiu");
 
 router.get("/", home);
 router.get("/baucua", baucua);
+router.get("/taixiu", taixiu);
 
 module.exports = router;
